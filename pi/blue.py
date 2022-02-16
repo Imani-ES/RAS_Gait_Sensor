c38a74aab635d02dae7847fecba1a038f281de4e
Imani-ES <imanimuh@buffalo.edu>
2022-02-16 13:31:57 -0500

    Update blue.py

--- pi/blue.py
+++ pi/blue.py
@@ -1,4 +1,5 @@
 #Resources
+#https://github.com/pyqt/examples
 #https://blog.kevindoran.co/bluetooth-programming-with-python-3/
 #for the desktop app, get address of pi and connect
 # for the pi, connect with any adress that pairs using hciconfig or bluetooth ctl show
