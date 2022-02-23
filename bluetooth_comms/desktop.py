cad88d62d7bdb2d65df02e7b0998f9157e2afaaa
Imani-ES <76416883+Imani-ES@users.noreply.github.com>
2022-02-23 11:53:27 -0500

    Update desktop.py

--- bluetooth_comms/desktop.py
+++ bluetooth_comms/desktop.py
@@ -1,4 +1,4 @@
-# for the desktop app, get address of pi and connect
+# for the desktop app, might need two 
 # not sure how to allow any device to pair without initial IP address though
 #have the desktop connect to two different hosts (Won't need multiple threads, have two pis)
 #
@@ -7,11 +7,12 @@ import socket
 
 # get addresses
 port = 3
-desk_addr = '?'#need to find with hconfig i believe
+pi_1_addr = '?'#need to find with hconfig i believe
+blue_pi_2 = '?'#need to find with hconfig i believe
 
-#establish bluetooth socket 
-blue_desk = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-blue_desk.connect((desk_addr,port))
+#establish bluetooth sockets 
+blue_pi_1 = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+blue_pi_1.connect((pi_1_addr,port))
 blue_desk.send(bytes('greetings from app'),'UFT-8')
 while 1:
     #input = data sent over socket
