cd189ec758e791cdfaadfef880d601302b5f1247
Imani-ES <76416883+Imani-ES@users.noreply.github.com>
2022-02-23 11:47:55 -0500

    Update raspberry.py

--- bluetooth_comms/raspberry.py
+++ bluetooth_comms/raspberry.py
@@ -1,21 +1,27 @@
 # for the pi, connect with any adress that pairs using hciconfig or bluetooth ctl show
-# not sure how to allow any device to pair without initial IP address though
+# have the pi host the connection
 
 import socket
-
-pi_addr= '?' # we can get  
+#global variables
+pi_addr= '?' # we can get from hciconfig
 port = 3 
 backlog = 1
 size = 1024
+
+#set up bluetooth socket server
 blue_pi = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 blue_pi.bind((pi_addr,port))
 blue_pi.listen(backlog)
+print("Blue_Pi listening at "+pi_addr+":"+port)
+
+#handle socket communication
 try:
     client, address = blue_pi.accept()
+    print("Connected to "+client)
     while 1:
         data = client.recv(size)
         if data:
-            print(data)
+            print("Recieved "+data +" from "+client)
             client.send(data)
 except:	
     print("Closing socket")	
