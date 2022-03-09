1ac5616d9f5511c17dd4c9b12b9f97ac9e1b1259
talamoandrew <54868781+talamoandrew@users.noreply.github.com>
2022-03-09 12:32:57 -0500

    Update desktop.py

--- bluetooth_comms/desktop.py
+++ bluetooth_comms/desktop.py
@@ -5,25 +5,32 @@
 
 import socket
 
+def lengthVsVoltage(x):
+    return (x + 0.0729) / 0.0329
+
+def normalLen(x):
+    l0 = 8.26
+    l90 = 10.88
+    return ((x - l0) / (l90-l0) )*90
+
 # get addresses
-port = 3
-pi_1_addr = '?'#need to find with hconfig i believe
+port = 4
+pi_1_addr = 'B8:27:EB:03:EB:6D'#need to find with hconfig i believe
 blue_pi_2 = '?'#need to find with hconfig i believe
 
 #establish bluetooth sockets 
 blue_pi_1 = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 blue_pi_1.connect((pi_1_addr,port))
-blue_pi_1.send(bytes('greetings from app','UFT-8'))
+blue_pi_1.send(bytes('greetings from app','UTF-8'))
+
 while 1:
-    #input = data sent over socket
-    text = input()
 
-    if text == "greeting":
-        print("Greeting from raspberry")
+    from_server = blue_pi_1.recv(4096)
+    from_server_d = from_server.decode()
 
-    elif text == "disconnect":
-        break
-    blue_pi_1.send(bytes(text, 'UTF-8'))
+
+    estLen = lengthVsVoltage(float(from_server_d))
+    print ('Voltage: ' + from_server_d + ' V | Estimated Length: ' + (str(estLen)) + ' | Estimated Angle: ' + (str(normalLen(estLen))))
 
 blue_pi_1.close()
 
