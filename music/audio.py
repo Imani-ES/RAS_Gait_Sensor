07affa3f9335387d6eaa59abb87a73b68c9ee526
Imani-ES <imanimuh@buffalo.edu>
2022-03-14 13:00:11 -0400

    added script to get songs

--- music/audio.py
+++ music/audio.py
@@ -1,4 +1,10 @@
-from pydub import AudioSegment
-import os
+#pip3 install pydub
+#pip3 install soundfile
+#pip3 install pyrubberband
+#pip3 install numpy
+#pip3 install AudioSegment
 
 #https://medium.com/prog-ramming-solutions/python-music-playback-speed-1x-2x-3x-change-without-chipmunk-effect-890eb10826c1
+
+from pydub import AudioSegment
+import os
