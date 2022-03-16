5161bd5e89f97edfd25f98b419b3054e15ef01da
Kexin Chen <kchen9988@me.com>
2022-03-16 14:12:35 -0400

    Added some audio stuff

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
+from os.path import exists
 
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtMultimedia import QAudioOutput, QAudio, QAudioFormat, QAudioDeviceInfo
+from PyQt5.QtCore import QFile, QIODevice
+
 
 def startApp():
     app = QGuiApplication(sys.argv)
@@ -13,6 +17,30 @@ def startApp():
     engine.load('./main.qml')
     
     #set up music
+    path = "./music/playlists/95-100_bpm/Lynyrd Skynyrd - Sweet Home Alabama.wav"
+    soundFilePath = exists(path)
+    #print(soundFilePath)
+
+    if soundFilePath:
+        soundFile = QFile()
+        soundFile.setFileName(path)
+        soundFile.open(QIODevice.OpenModeFlag.ReadOnly)
+
+        format = QAudioFormat()
+        format.setSampleRate(8000)
+        format.setChannelCount(1)
+        format.setSampleSize(8)
+        format.setCodec("audio/pcm")
+        format.setByteOrder(QAudioFormat.Endian.LittleEndian)
+        format.setSampleType(QAudioFormat.SampleType.UnSignedInt)
+
+        info = QAudioDeviceInfo(QAudioDeviceInfo.defaultOutputDevice())
+        if not info.isFormatSupported(format):
+            print("Raw audio format not supported, cannot play.")
+
+        output = QAudioOutput(format)
+        output.start(soundFile)
+        
 
     sys.exit(app.exec())
 
